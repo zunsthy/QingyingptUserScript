bfd5dc3991e0c336d8c58bfac81dd42ea30ceab1
zunsthy <zunsthy@gmail.com>
2015-03-29 11:15:11 +0800

    meta.js

---
+++ QptUserScript_Transmit.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        QptUserScript_Transmit
+// @namespace   User Script for QingyingPT
+// @description	nothing useful
+// @version     0.54.2015.03281100
+// @author      ZunSThy
+// @updateURL   https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Transmit.meta.js
+// @downloadURL https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Transmit.user.js
+// @icon        
+// @icon64      
+// @domain      pt.hit.edu.cn
+// @match       http://pt.hit.edu.cn/edit.php*
+// @match       http://pt.hit.edu.cn/upload.php
+// @match       http://pt.hit.edu.cn/uploadnew.php
+// @include     http://pt.hit.edu.cn/edit.php*
+// @include     http://pt.hit.edu.cn/upload.php
+// @include     http://pt.hit.edu.cn/uploadnew.php
+// @grant       GM_xmlhttpRequest
+// @require     http://pt.hit.edu.cn/jquerylib/jquery-1.7.2.min.js
+// @copyright   2014-2015
+// ==/UserScript==
