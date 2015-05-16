786eff92c1c06f9423f75a1c2ee9407d455ffd89
zunsthy <zunsthy@gmail.com>
2015-05-16 17:14:56 +0800

    update version number

--- QptUserScript_Transmit.meta.js
+++ QptUserScript_Transmit.meta.js
@@ -2,7 +2,7 @@
 // @id          Qptuserscript_Transmit
 // @name        QptUserScript Transmit
 // @author      ZunSThy <zunsthy@gmail.com>
-// @version     0.5.5.20150430.135
+// @version     0.5.5.20150515.713
 // @namespace   https://github.com/zunsthy/QingyingptUserScript
 // @updateURL   https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Transmit.meta.js
 // @downloadURL https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Transmit.user.js
