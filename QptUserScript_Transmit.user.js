5f743851a2de00a90d8935705e7f3dd302413dee
ZunSThy <zunsthy@users.noreply.github.com>
2015-12-13 00:55:41 +0800

    Update QptUserScript_Transmit.user.js

--- QptUserScript_Transmit.user.js
+++ QptUserScript_Transmit.user.js
@@ -7,13 +7,18 @@
 // @updateURL   https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Transmit.meta.js
 // @downloadURL https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Transmit.user.js
 // @description nothing useful
-// @domain      pt.hit.edu.cn
 // @match       http://pt.hit.edu.cn/edit.php*
 // @match       http://pt.hit.edu.cn/upload.php
 // @match       http://pt.hit.edu.cn/uploadnew.php
+// @match       https://pt.hit.edu.cn/edit.php*
+// @match       https://pt.hit.edu.cn/upload.php
+// @match       https://pt.hit.edu.cn/uploadnew.php
 // @include     http://pt.hit.edu.cn/edit.php*
 // @include     http://pt.hit.edu.cn/upload.php
 // @include     http://pt.hit.edu.cn/uploadnew.php
+// @include     https://pt.hit.edu.cn/edit.php*
+// @include     https://pt.hit.edu.cn/upload.php
+// @include     https://pt.hit.edu.cn/uploadnew.php
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
