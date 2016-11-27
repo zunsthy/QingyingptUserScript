c03373061bb32e1cf33f4733fd0b3af8dd7a7f67
zunsthy <zunsthy@gmail.com>
2016-11-27 18:16:59 +0800

    update forums script version to 0.1.332.1818

--- QptUserScript_Forum.meta.js
+++ QptUserScript_Forum.meta.js
@@ -2,7 +2,7 @@
 // @id          Qptuserscript_Forum@ZunSThy
 // @name        QptUserScript Forum
 // @author      ZunSThy <zunsthy@gmail.com>
-// @version     0.1.264.1010
+// @version     0.1.332.1818
 // @namespace   https://github.com/zunsthy/QingyingptUserScript
 // @updateURL   https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Forum.meta.js
 // @downloadURL https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Forum.user.js
