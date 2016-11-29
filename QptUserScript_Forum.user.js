bacbe302b9038373b1d3c4e622d79d946ee08cba
zunsthy <zunsthy@gmail.com>
2016-11-29 23:34:01 +0800

    update forums 0.1.334.2333

--- QptUserScript_Forum.user.js
+++ QptUserScript_Forum.user.js
@@ -2,7 +2,7 @@
 // @id          Qptuserscript_Forum@ZunSThy
 // @name        QptUserScript Forum
 // @author      ZunSThy <zunsthy@gmail.com>
-// @version     0.1.332.1818
+// @version     0.1.334.2333
 // @namespace   https://github.com/zunsthy/QingyingptUserScript
 // @updateURL   https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Forum.meta.js
 // @downloadURL https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/QptUserScript_Forum.user.js
@@ -17,7 +17,7 @@
 (function(){
 'use strict';
 const prefix = 'qpt',
-      ver = '0.1.264.1010';
+      ver = '0.1.334.2333';
 
 const head = document.head || document.getElementsByTagName('head')[0],
       body = document.body;
