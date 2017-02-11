867a828a55e5b6d4a2c9c1276390f45ab8c02bb9
zunsthy <zunsthy@gmail.com>
2017-02-11 23:03:34 +0800

    fix locked display error

--- QptUserScript_Forum.user.js
+++ QptUserScript_Forum.user.js
@@ -19,7 +19,7 @@
 
 const suffix = 'QPT';
 const coder = 'zunsthy';
-const ver = '0.2.42.0';
+const ver = '0.2.42';
 
 const head = document.head || document.getElementsByTagName('head');
 const body = document.body;
@@ -2581,7 +2581,7 @@ ${more ? ('<a class="op-button viewmore" href="' + linkViewMoreUnread + lastpost
   <div class="space vline">
     <div class="space line">
       <div class="item topic-status-area">
-        ${locked ? '<div class="topic-status locked" title="锁定"></div>' : ''}
+        ${locked === 'yes' ? '<div class="topic-status locked" title="锁定"></div>' : ''}
         <div class="topic-status unread" title="未读"></div>
       </div>
       <div class="space text-left">
