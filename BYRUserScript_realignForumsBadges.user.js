45228ba6e0105e7ad375ce2257a3a84b58ff4425
zunsthy <zunsthy@gmail.com>
2018-06-02 18:31:42 +0800

    bry script support https

--- BYRUserScript_realignForumsBadges.user.js
+++ BYRUserScript_realignForumsBadges.user.js
@@ -4,9 +4,11 @@
 // @author      ZunSThy <zunsthy@gmail.com>
 // @namespace   https://github.com/zunsthy/QingyingptUserScript
 // @description re-align the dadges if odd number
+// @include     https://bt.byr.cn/forums.php?*
 // @include     http://bt.byr.cn/forums.php?*
+// @match       https://bt.byr.cn/forums.php?*
 // @match       http://bt.byr.cn/forums.php?*
-// @version     0.2
+// @version     0.2.1
 // @updateURL   https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/BYRUserScript_realignForumsBadges.meta.js
 // @downloadURL https://raw.githubusercontent.com/zunsthy/QingyingptUserScript/master/BYRUserScript_realignForumsBadges.user.js
 // @grant       none
